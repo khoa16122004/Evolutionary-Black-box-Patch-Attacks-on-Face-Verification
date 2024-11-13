02fdb177ce3e54e636c7ba45bb32daf097a18560
khoa16122004 <khoatr.work@gmail.com>
2024-11-13 22:19:50 +0700

    fix

--- test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import cv2 as cv
-
-img = cv.resize(cv.imread("Aaron_Sorkin_0002.jpg"), (160, 160))
-patch = cv.imread("output_best.jpg")
-print(patch.shape)
-img[80: 120, 80: 120, :] = patch
-cv.imwrite("img_patch.png", img)
